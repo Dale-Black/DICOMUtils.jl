e4bff507e3ad0c0ccbf650b3ad84cb3e34438928
Dale Black <djblack@uci.edu>
2021-05-27 17:24:26 -0700

    return just the array

--- src/load.jl
+++ src/load.jl
@@ -1,10 +1,8 @@
 """
     load_dcm_array(dcm_data::Vector{DICOM.DICOMData})
-Given DICOM.DICOMData, `load_dcm_array` loads the pixels
-into a 3D array and returns a tuple of the 3D array and the 
-original DICOM.DICOMData
+Given some DICOM.DICOMData, `load_dcm_array` loads the pixels
+of each slice into a 3D array and returns the array
 """
 function load_dcm_array(dcm_data::Vector{DICOM.DICOMData})
-	arr = cat([dcm_data[i][(0x7fe0,0x0010)] for i = 1:length(dcm_data)]..., dims=3)
-	return (arr, dcm_data)
+	array = cat([dcm_data[i][(0x7fe0,0x0010)] for i = 1:length(dcm_data)]..., dims=3)
 end
