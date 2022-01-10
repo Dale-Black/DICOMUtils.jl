2b1f6a65d4f67935d4c6ba6f1f08e4b9c2fc333f
Dale-Black <djblack@uci.edu>
2022-01-10 13:57:23 -0800

    update

--- src/load.jl
+++ src/load.jl
@@ -10,7 +10,6 @@ function load_dcm_array(dcm_data::Vector{DICOM.DICOMData})
 end
 
 """
-WARNING - BROKEN, NEEDS ADJUSTMENT
     get_pixel_size(header)
 
 Get the pixel information of the DICOM image given the `header` info.
@@ -18,16 +17,17 @@ Returns the x, y, and z values, where `z` corresponds to slice thickness
 
 """
 function get_pixel_size(header)
+	head = copy(header)
 	pixel_size = 
 		try
-			header[(0x0028, 0x0030)]
-            push!(pixel_size, header[(0x0018, 0x0050)])
+			pixel_size = (head[(0x0028, 0x0030)])
+            push!(pixel_size, head[(0x0018, 0x0050)])
 		catch
-			FOV = header[(0x0018, 0x1100)]
-			matrix_size = header[(0x0028, 0x0010)]
+			FOV = (head[(0x0018, 0x1100)])
+			matrix_size = head[(0x0028, 0x0010)]
 		
 			pixel_size = FOV / matrix_size
-            push!(pixel_size, header[(0x0018, 0x0050)])
+            push!(pixel_size, head[(0x0018, 0x0050)])
 		end
 	return pixel_size
 end
